dfd47ae5ed2249beb3a2ba2dacc3940fc31aecc6
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-05-09 10:13:06 +0530

    Added Comments of Transaction in Booking Service.

--- src/main/java/com/bms/bookmyshow/models/BaseModel.java
+++ src/main/java/com/bms/bookmyshow/models/BaseModel.java
@@ -15,7 +15,8 @@ import java.util.Date;
 This base model is super class used to set common attributes to all the table.
 Remember Why Jaspreet always told to use this.
 
-@MappedSuperclass It tells JPA that whichever class is extending this class, make these attributes available as column in sub-class tables.
+@MappedSuperclass It tells JPA that whichever class is extending this class, make these attributes available
+as column in subclass tables.
  */
 
 @Getter
--- src/main/java/com/bms/bookmyshow/services/BookingService.java
+++ src/main/java/com/bms/bookmyshow/services/BookingService.java
@@ -2,16 +2,52 @@ package com.bms.bookmyshow.services;
 
 import com.bms.bookmyshow.models.Booking;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class BookingService {
 
-    //Here We return Booking object and BookingResponseDto cauz this service can be called by many Controllers having different-different DTOs.
-    //Therefore we donot tightcouple service method with any retun dtos
+    /*
+     Here We return Booking object and BookingResponseDto cauz this service can be called by many Controllers having different-different DTOs.
+     Therefore we donot tightcouple service method with any return type dtos
+     */
 
+    /*
+      In java code, we use synchronized keyword to take lock on particular method, but this synchronized will
+      not work in DB part. We have to use something that works in DB transaction to take lock.
+      @Transactional: works for that
+     */
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public Booking bookMovie(Long userId, Long showId, List<Long> showSeatIds){
+
+        //Multiple people should not be able to book same seat at same time. That is concurrency Locking technique
+
+        /*
+        Implement Booking Logic
+        1) Fetch User from Db using userId
+        2) Fetch show from DB using showId
+        3) Get the list of showSeats from showSeatIds from DB
+        4) Check If All seats Available or not
+        ------------ACQUIRE LOCK -------------
+        4.1) If Not, Throw an exception
+        4.2) If yes, Mark the status as BLOCKED.
+        5) Save the status of seat in DB as well
+        ------------RELEASE LOCK -------------
+        6) Create Booking Object.
+        7) Save the Booking to Db
+        8) Return the booking
+
+        Now , In Springboot, acquiring lock at any midpoint in code is difficult task. Therefore, we will move ahead
+        with the FIRST approach and Lock whole method of booking (cauz lock whole method is very easy task in Spring Boot)
+
+         */
+
+
+
 
         return null;
 
